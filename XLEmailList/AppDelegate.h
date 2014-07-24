0fa537e0590952493c7c772423cfb8219c018182
员员 <syxiaqj@gmail.com>
2014-07-24 14:31:29 +0800

    Initial Commit

---
+++ XLEmailList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XLEmailList
+//
+//  Created by xqj on 14-7-24.
+//  Copyright (c) 2014年 renhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
